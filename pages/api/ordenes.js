ed209b960faa6fe77374a9621e3ddfe82b2df65a
Erickrova <crachipp01@gmail.com>
2022-09-30 18:28:04 -0500

    1.1.1

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -5,7 +5,7 @@ export default async function handler (req,res){
     const prisma = new PrismaClient()
 
     if(req.method === "POST"){
-        const orden = await prisma.orden.create({
+        const Orden = await prisma.orden.create({
             data:{
                 nombre: req.body.nombre,
                 fecha: req.body.fecha,
@@ -13,9 +13,6 @@ export default async function handler (req,res){
                 pedido: req.body.pedido,
             },
         })
-        res.json(orden)
-    }else{
-        
-        res.status(200).json({ordenes: "aqui iran las ordenes"})
+        res.json(Orden)
     }
 }
